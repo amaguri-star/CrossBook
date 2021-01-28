c12086d5ca467e25c9f4897aa9ce83f583160d62
Ryusei <ryusei1597@gmail.com>
2021-01-28 22:19:17 +0900

    add singup page

--- crossbook/views.py
+++ crossbook/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm
 
 
 # Create your views here.
 def home(request):
-    context = {}
-    return render(request, "crossbook/home.html", context)
+    return render(request, 'crossbook/home.html')
+
+
+def signup(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+
+    context = {'form': form}
+    return render(request, 'crossbook/signup.html', context)
+
+
