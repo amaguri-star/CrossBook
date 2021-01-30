1aba695abce40a2be3f7f84dc8ac45681be671b4
Ryusei <ryusei1597@gmail.com>
2021-01-31 01:30:46 +0900

    add navbar

--- crossbook/urls.py
+++ crossbook/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("signup/", views.signup, name="signup"),
     path("login/", views.login, name="login"),
+    path("logout/", views.logout, name="logout"),
+
 ]
--- crossbook/views.py
+++ crossbook/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from .forms import CreateUserForm
 
 
@@ -9,11 +11,12 @@ def home(request):
 
 def signup(request):
     form = CreateUserForm()
-
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, username + "のアカウントが作成されました")
             return redirect('login')
 
     context = {'form': form}
@@ -21,5 +24,22 @@ def signup(request):
 
 
 def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'ユーザー名かパスワードのどちらかが間違っています。')
     context = {}
     return render(request, 'crossbook/login.html', context)
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect('login')
+
+
+
